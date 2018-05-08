4aa6b516978a7169627bdc9bf76a4f5bb7541541
china <shidenov_chingiz@mail.ru>
2018-05-08 13:31:16 +0600

    automatization

--- gulpfile.js
+++ gulpfile.js
@@ -77,7 +77,8 @@ gulp.task("copy", function() {
   return gulp.src([
     "source/fonts/**/*.{woff,woff2}",
     "source/img/**",
-    "source/js/**"
+    "source/js/**",
+    "source/*.html"
     ], {
     base: "source"
     })
